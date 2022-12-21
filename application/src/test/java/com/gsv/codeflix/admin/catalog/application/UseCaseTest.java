f27a31656a0262437c83a0945bffe0b09ab7fb16
Guilherme <silva.guilherme09@gmail.com>
2022-12-21 14:03:44 -0300

    Add Mockito dependency

--- application/src/test/java/com/gsv/codeflix/admin/catalog/application/UseCaseTest.java
+++ application/src/test/java/com/gsv/codeflix/admin/catalog/application/UseCaseTest.java
@@ -1,12 +1,12 @@
 package com.gsv.codeflix.admin.catalog.application;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class UseCaseTest {
     @Test
     public void testCreateUseCase() {
-        Assertions.assertNotNull(new UseCase());
-        Assertions.assertNotNull(new UseCase().execute());
     }
 }
