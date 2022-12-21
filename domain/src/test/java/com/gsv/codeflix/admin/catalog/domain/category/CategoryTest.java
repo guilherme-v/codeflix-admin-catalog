fc415f72b9a8c2b7676c99ebf1a6c9ea2dc929d3
Guilherme <silva.guilherme09@gmail.com>
2022-12-20 22:50:03 -0300

    Fix CategoryTest

--- domain/src/test/java/com/gsv/codeflix/admin/catalog/domain/category/CategoryTest.java
+++ domain/src/test/java/com/gsv/codeflix/admin/catalog/domain/category/CategoryTest.java
@@ -1,21 +1,20 @@
 package com.gsv.codeflix.admin.catalog.domain.category;
 
+import com.gsv.codeflix.admin.catalog.domain.exceptions.DomainException;
+import com.gsv.codeflix.admin.catalog.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CategoryTest {
 
     @Test
-    public void testNewCategory() {
-        // Given
+    public void givenValidParams_whenCallNewCategory_thenReturnACategory() {
         final var name = "MOVIE_XYZ";
         final var desc = "Description of the movie";
         final var isActive = true;
 
-        // When
         final var actual = Category.newCategory(name, desc, isActive);
 
-        // Then
         Assertions.assertNotNull(actual);
         Assertions.assertNotNull(actual.getId());
         Assertions.assertEquals(name, actual.getName());
@@ -24,5 +23,17 @@ class CategoryTest {
         Assertions.assertNotNull(actual.getCreatedAt());
         Assertions.assertNotNull(actual.getUpdatedAt());
         Assertions.assertNull(actual.getDeletedAt());
+    }
+
+    @Test
+    public void givenNullName_whenCallNewCategory_thenThroughException() {
+        final var desc = "Description of the movie";
+        final var isActive = true;
+
+        final var actual = Category.newCategory(null, desc, isActive);
+
+        final var actualExp = Assertions.assertThrows(DomainException.class, () -> actual.validate(new ThrowsValidationHandler()));
+        Assertions.assertEquals(1, actualExp.getErrors().size());
+        Assertions.assertEquals("'name' should not be null", actualExp.getErrors().get(0).message());
     }
 }
