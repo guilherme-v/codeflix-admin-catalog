eeb3b6e64ab50506217b8ffb61f5d9084e987ea7
Guilherme <silva.guilherme09@gmail.com>
2022-12-29 15:46:50 -0300

    Include H2 and test profile

--- infrastructure/src/test/java/com/gsv/codeflix/admin/catalog/infrastructure/MainTest.java
+++ infrastructure/src/test/java/com/gsv/codeflix/admin/catalog/infrastructure/MainTest.java
@@ -1,13 +1,15 @@
 package com.gsv.codeflix.admin.catalog.infrastructure;
 
-import org.junit.jupiter.api.Assertions;
+import com.gsv.codeflix.admin.catalog.infrastructure.configuration.WebServerConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 class MainTest {
 
     @Test
     public void testMain() {
-        Assertions.assertNotNull(new Main());
-        Main.main(new String[]{});
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "test");
+        SpringApplication.run(WebServerConfig.class, new String[]{});
     }
 }
