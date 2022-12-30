883c76229e656586b0c92e3e896a1b626856b64b
Guilherme <silva.guilherme09@gmail.com>
2022-12-30 16:27:32 -0300

    Create CategoryJpaEntity

--- domain/src/main/java/com/gsv/codeflix/admin/catalog/domain/category/Category.java
+++ domain/src/main/java/com/gsv/codeflix/admin/catalog/domain/category/Category.java
@@ -30,6 +30,38 @@ public class Category extends AggregateRoot<CategoryID> implements Cloneable {
         return new Category(id, name, desc, isActive, timestamp, timestamp, null);
     }
 
+    public static Category with(
+            final CategoryID anId,
+            final String name,
+            final String description,
+            final boolean active,
+            final Instant createdAt,
+            final Instant updatedAt,
+            final Instant deletedAt
+    ) {
+        return new Category(
+                anId,
+                name,
+                description,
+                active,
+                createdAt,
+                updatedAt,
+                deletedAt
+        );
+    }
+
+    public static Category with(final Category aCategory) {
+        return with(
+                aCategory.getId(),
+                aCategory.name,
+                aCategory.description,
+                aCategory.isActive,
+                aCategory.createdAt,
+                aCategory.updatedAt,
+                aCategory.deletedAt
+        );
+    }
+
     public CategoryID getId() {
         return id;
     }
